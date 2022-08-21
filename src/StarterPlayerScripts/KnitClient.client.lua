8c8123876828aa6b4fe8d9637ccf80cf0bd7f205
vkara <vasildata@gmail.com>
2022-08-21 18:06:40 -0400

    Load UIs before add Controllers

--- src/StarterPlayerScripts/KnitClient.client.lua
+++ src/StarterPlayerScripts/KnitClient.client.lua
@@ -1,5 +1,18 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+-- No longer need to :wait() for UIs to load
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+local UIs = ReplicatedStorage.UI
+local playerGui = game:GetService("Players").LocalPlayer.PlayerGui
+
+for _, screenGui in ipairs(UIs:GetChildren()) do
+	screenGui:Clone().Parent = playerGui
+end
+--
+
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 Knit.AddControllers(ReplicatedStorage.Controllers)
