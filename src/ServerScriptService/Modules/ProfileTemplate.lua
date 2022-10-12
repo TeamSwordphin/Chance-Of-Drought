1cc4022a9e7448f8dfbb6389de1678e5676b1f62
vkara <vasildata@gmail.com>
2022-10-12 18:30:32 -0400

    Update ProfileTemplate.lua

--- src/ServerScriptService/Modules/ProfileTemplate.lua
+++ src/ServerScriptService/Modules/ProfileTemplate.lua
@@ -1,3 +1,14 @@
-local ProfileTemplate = {}
+local ProfileTemplate = {
+	Coins = 0,
+	ItemsPool = {},
+	CharacterPool = {},
+	XP = 0,
+	Achievements = {},
+	Ban = {
+		Banned = false,
+		Reason = "",
+		Score = 0,
+	},
+}
 
 return ProfileTemplate
